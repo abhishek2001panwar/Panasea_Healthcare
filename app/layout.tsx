1655cbde29270eef1ef3d8316ba66ba0aedb1d2c
GAUTAM MANAK <106014185+gautammanak1@users.noreply.github.com>
2026-04-08 11:37:02 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
+import './globals.css'
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+
+export const metadata: Metadata = {
+  title: 'Panasea Healthcare',
+  description: 'Panasea Healthcare is a leading provider of innovative healthcare solutions, dedicated to improving patient outcomes and enhancing the quality of care through cutting-edge technology and compassionate service.',
+  generator: '',
+  icons: {
+    icon: [
+      {
+        url: '/icon-light-32x32.png',
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        url: '/icon-dark-32x32.png',
+        media: '(prefers-color-scheme: dark)',
+      },
+      {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+      },
+    ],
+    apple: '/apple-icon.png',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} font-sans antialiased`}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  )
+}

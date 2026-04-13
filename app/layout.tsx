69f2f079dc935ced13675ba403cb1c7d98c1f9a1
GAUTAM MANAK <106014185+gautammanak1@users.noreply.github.com>
2026-04-13 11:18:47 +0530

    adds new data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
-import React from "react"
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
-import Navbar from "@/components/navbar"
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
+import Navbar from "@/components/navbar";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-  title: 'Panasea Healthcare',
-  description: 'Panasea Healthcare is a leading provider of innovative healthcare solutions, dedicated to improving patient outcomes and enhancing the quality of care through cutting-edge technology and compassionate service.',
-
-}
+  title:
+    "Panasea Healthcare | Medical Supplies & Pharmaceutical Distributor in India",
+  description:
+    "Your trusted partner in healthcare distribution. Panasea Healthcare supplies medicines, medical equipment, and hospital essentials with fast, safe, and efficient delivery across India.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
-         <Navbar />
+        <Navbar />
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
